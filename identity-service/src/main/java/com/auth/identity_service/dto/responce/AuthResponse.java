92afb6361de2a55dd3a72d034e74a310624d197b
Le Bui Tien Thuan <lebuithuan@gmail.com>
2025-12-08 23:05:38 +0700

    feat(identity): create AuthResponse DTO

---
+++ identity-service/src/main/java/com/auth/identity_service/dto/responce/AuthResponse.java
@@ -0,0 +1,11 @@
+package com.auth.identity_service.dto.responce;
+
+import lombok.*;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+}
