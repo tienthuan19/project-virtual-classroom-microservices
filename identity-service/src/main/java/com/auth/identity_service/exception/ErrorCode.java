98ebb54837393c7eb6635cfd7cc28bc02a02abd2
Le Bui Tien Thuan <lebuithuan@gmail.com>
2025-12-09 23:20:13 +0700

    feat(exception): implement custom AppException and ErrorCode enum for standardized error handling

---
+++ identity-service/src/main/java/com/auth/identity_service/exception/ErrorCode.java
@@ -0,0 +1,23 @@
+package com.auth.identity_service.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+    UNCATEGORIZED_EXCEPTION(9999, "Uncategorized error", HttpStatus.INTERNAL_SERVER_ERROR),
+    USER_EXISTED(1001, "User existed", HttpStatus.BAD_REQUEST),
+    USER_NOT_FOUND(1002, "User not found", HttpStatus.NOT_FOUND),
+    UNAUTHENTICATED(1003, "Unauthenticated", HttpStatus.UNAUTHORIZED), // 401
+    UNAUTHORIZED(1004, "You do not have permission", HttpStatus.FORBIDDEN); // 403
+
+    private int code;
+    private String message;
+    private HttpStatus statusCode; 
+
+    ErrorCode(int code, String message, HttpStatus statusCode) {
+        this.code = code;
+        this.message = message;
+        this.statusCode = statusCode;
+    }
+}
