b8953fab10eda7cfcb56d00fea2ddffdcd761b77
Le Bui Tien Thuan <lebuithuan@gmail.com>
2025-12-10 23:43:38 +0700

    feat: add getUserByEmail method

--- identity-service/src/main/java/com/auth/identity_service/services/UserService.java
+++ identity-service/src/main/java/com/auth/identity_service/services/UserService.java
@@ -2,6 +2,8 @@ package com.auth.identity_service.services;
 
 import org.springframework.stereotype.Service;
 
+import com.auth.identity_service.exception.AppException;
+import com.auth.identity_service.exception.ErrorCode;
 import com.auth.identity_service.models.User;
 import com.auth.identity_service.repository.UserRepository;
 
@@ -14,5 +16,11 @@ public class UserService {
 
     public void createUser(User user) {
         userRepository.save(user);
+    }
+
+    public User getUserByEmail(String email) {
+        User user = userRepository.findByEmail(email)
+            .orElseThrow(() -> new AppException(ErrorCode.USER_NOT_FOUND));
+        return user;
     }
 }
