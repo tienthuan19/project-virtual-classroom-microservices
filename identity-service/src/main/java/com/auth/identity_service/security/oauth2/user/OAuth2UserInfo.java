691c2159b46077562346696f8593b851ce8f4153
Le Bui Tien Thuan <lebuithuan@gmail.com>
2025-12-11 19:35:43 +0700

    feat(identity/oauth2): add abstract OAuth2UserInfo base class

---
+++ identity-service/src/main/java/com/auth/identity_service/security/oauth2/user/OAuth2UserInfo.java
@@ -0,0 +1,15 @@
+package com.auth.identity_service.security.oauth2.user;
+
+import java.util.Map;
+
+public abstract class OAuth2UserInfo {
+    protected Map<String, Object> attributes;
+
+    public OAuth2UserInfo(Map<String, Object> attributes) {
+        this.attributes = attributes;
+    }
+
+    public abstract String getId();
+    public abstract String getName();
+    public abstract String getEmail();
+}
