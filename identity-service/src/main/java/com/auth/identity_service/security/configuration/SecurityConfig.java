b25e6338237fc49d707342ae1432fd4ba829f6c6
Le Bui Tien Thuan <lebuithuan@gmail.com>
2025-12-15 13:09:52 +0700

    refactor(security): move SecurityConfig to security/configuration

---
+++ identity-service/src/main/java/com/auth/identity_service/security/configuration/SecurityConfig.java
@@ -0,0 +1,48 @@
+package com.auth.identity_service.security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.auth.identity_service.security.oauth2.CustomOAuth2UserService;
+import com.auth.identity_service.security.oauth2.OAuth2LoginSuccessHandler;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration("securityConfigV2")
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+    private final OAuth2LoginSuccessHandler oAuth2LoginSuccessHandler;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) {
+        http
+            .csrf(AbstractHttpConfigurer::disable) 
+            .authorizeHttpRequests(request -> request
+                
+                .requestMatchers("/api/identity-service/v1/auth/**").permitAll() 
+                
+                .anyRequest().authenticated()
+            )
+            .oauth2Login(oauth2 -> oauth2
+                .userInfoEndpoint(userInfo -> userInfo
+                    .userService(customOAuth2UserService)
+                )
+                .successHandler(oAuth2LoginSuccessHandler) 
+            );
+        return http.build();
+    }
+}
