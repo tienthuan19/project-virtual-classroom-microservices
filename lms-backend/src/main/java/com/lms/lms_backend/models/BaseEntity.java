5e776eab4d0817c8e1c462d7048389688bf85b99
Le Bui Tien Thuan <lebuithuan@gmail.com>
2025-12-18 19:44:25 +0700

    feat(lms): setup database dependencies and add Class entity

---
+++ lms-backend/src/main/java/com/lms/lms_backend/models/BaseEntity.java
@@ -0,0 +1,24 @@
+package com.lms.lms_backend.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
---
+++ lms-backend/src/main/java/com/lms/lms_backend/models/Class.java
@@ -0,0 +1,37 @@
+package com.lms.lms_backend.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.Instant;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "classes")
+public class Class extends BaseEntity {
+    @Id
+    @jakarta.validation.constraints.Size(max = 36)
+    @Column(name = "id", nullable = false, length = 36)
+    private String id;
+
+    @jakarta.validation.constraints.Size(max = 10)
+    @jakarta.validation.constraints.NotNull
+    @Column(name = "class_code", nullable = false, length = 10)
+    private String classCode;
+
+    @jakarta.validation.constraints.Size(max = 255)
+    @jakarta.validation.constraints.NotNull
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "description", length = Integer.MAX_VALUE)
+    private String description;
+
+    @Column(name = "creator_id", length = Integer.MAX_VALUE)
+    private String creatorId;
+}
