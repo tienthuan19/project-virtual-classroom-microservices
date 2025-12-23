033dd1dd4a09d33a0212f6963a0474254607da7e
Le Bui Tien Thuan <lebuithuan@gmail.com>
2025-12-23 19:43:19 +0700

    feat(lms/model): add ClassMember entity for manage members of classroom

---
+++ lms-backend/src/main/java/com/lms/lms_backend/models/ClassMember.java
@@ -0,0 +1,31 @@
+package com.lms.lms_backend.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "class_members", uniqueConstraints = {
+        @UniqueConstraint(columnNames = {"class_code", "user_id"})
+})
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ClassMember extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "class_code", referencedColumnName = "class_code", nullable = false)
+    private Classroom classroom;
+
+    @Column(name = "user_id", nullable = false)
+    private String userId;
+
+    @Column(name = "joined_at")
+    private LocalDateTime joinedAt;
+}
