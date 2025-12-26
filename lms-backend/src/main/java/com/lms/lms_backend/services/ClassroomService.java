23a265df15b7c9ea17853d3d8452e7cfff16bc0e
Le Bui Tien Thuan <lebuithuan@gmail.com>
2025-12-26 21:36:08 +0700

    refactor(lms/ClassroomService): refactor code

--- lms-backend/src/main/java/com/lms/lms_backend/services/ClassroomService.java
+++ lms-backend/src/main/java/com/lms/lms_backend/services/ClassroomService.java
@@ -15,21 +15,14 @@ public class ClassroomService {
     private final ClassroomRepository classroomRepository;
 
     public ClassroomResponse createClassroom(ClassroomRequest request) {
-        // 1. Lấy ID của User đang đăng nhập từ SecurityContext
-        // (Do JwtAuthenticationFilter đã set vào trước đó)
+
         var context = SecurityContextHolder.getContext();
         String currentUserId = context.getAuthentication().getName();
-        // Lưu ý: .getName() sẽ trả về cái mình set ở "Principal" trong Filter.
-        // Nếu trong Filter bạn set Principal là userId -> nó ra userId.
-        // Nếu set là username -> nó ra username. (Nên set userId để lưu DB cho chuẩn).
 
-        // 2. Check trùng mã lớp
-        // Lưu ý: request.getCode() hay getClassCode() phụ thuộc vào cách bạn đặt tên trong DTO
         if (classroomRepository.existsByClassCode(request.getCode())) {
             throw new RuntimeException("Classroom with code " + request.getCode() + " already exists");
         }
 
-        // 3. Map Request -> Entity
         Classroom newClassroom = Classroom.builder()
                 .name(request.getName())
                 .classCode(request.getCode())
@@ -40,7 +33,6 @@ public class ClassroomService {
 
         Classroom savedClass = classroomRepository.save(newClassroom);
 
-        // 5. Map Entity -> Response
         return ClassroomResponse.builder()
                 .id(savedClass.getId())
                 .name(savedClass.getName())
