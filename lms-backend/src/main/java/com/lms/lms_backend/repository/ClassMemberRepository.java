389c63d167bbf49833edb1205fdf019c2fbd870d
Le Bui Tien Thuan <lebuithuan@gmail.com>
2025-12-27 20:11:23 +0700

    feat(lms/ClassMemberRepository): add find Student number with teacher ID who create that Classroom

--- lms-backend/src/main/java/com/lms/lms_backend/repository/ClassMemberRepository.java
+++ lms-backend/src/main/java/com/lms/lms_backend/repository/ClassMemberRepository.java
@@ -1,7 +1,16 @@
 package com.lms.lms_backend.repository;
 
+import com.lms.lms_backend.models.ClassMember;
+import com.lms.lms_backend.models.Classroom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class ClassMemberRepository {
+public interface ClassMemberRepository extends JpaRepository<ClassMember, Long> {
+    @Query("SELECT COUNT(cm) FROM ClassMember cm " +
+            "JOIN cm.classroom c " +
+            "WHERE c.creatorId = :creatorId")
+    long countTotalStudentsByTeacher(@Param("creatorId") String creatorId);
 }
