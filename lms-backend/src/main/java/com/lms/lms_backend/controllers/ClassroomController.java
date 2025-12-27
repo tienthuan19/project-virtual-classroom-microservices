27ebe13d27051a4d4e332329782ffb11d01ed1af
Le Bui Tien Thuan <lebuithuan@gmail.com>
2025-12-27 20:15:54 +0700

    feat(lms/ClassroomController): add /classrooms/teacher/my-classes & /classrooms/teacher/dashboard-stats

--- lms-backend/src/main/java/com/lms/lms_backend/controllers/ClassroomController.java
+++ lms-backend/src/main/java/com/lms/lms_backend/controllers/ClassroomController.java
@@ -2,14 +2,15 @@ package com.lms.lms_backend.controllers;
 
 import com.lms.lms_backend.dto.request.ClassroomRequest;
 import com.lms.lms_backend.dto.response.ApiResponse;
+import com.lms.lms_backend.dto.response.ClassroomCardResponse;
 import com.lms.lms_backend.dto.response.ClassroomResponse;
+import com.lms.lms_backend.dto.response.TeacherDashboardResponse;
 import com.lms.lms_backend.services.ClassroomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/lms-backend/v1")
@@ -23,6 +24,27 @@ public class ClassroomController {
         ClassroomResponse response = classroomService.createClassroom(classroomRequest);
         return ApiResponse.<ClassroomResponse>builder()
                 .status(200)
+                .data(response)
+                .build();
+    }
+    @GetMapping("/classrooms/teacher/my-classes")
+    @PreAuthorize("hasRole('TEACHER')")
+    public ApiResponse<List<ClassroomCardResponse>> getMyClassrooms() {
+        List<ClassroomCardResponse> response = classroomService.getTeacherClassrooms();
+        return ApiResponse.<List<ClassroomCardResponse>>builder()
+                .status(200)
+                .message("Get classroom list successfully")
+                .data(response)
+                .build();
+    }
+
+    @GetMapping("/classrooms/teacher/dashboard-stats")
+    @PreAuthorize("hasRole('TEACHER')")
+    public ApiResponse<TeacherDashboardResponse> getDashboardStats() {
+        TeacherDashboardResponse response = classroomService.getTeacherDashboardStats();
+        return ApiResponse.<TeacherDashboardResponse>builder()
+                .status(200)
+                .message("Get dashboard stats successfully")
                 .data(response)
                 .build();
     }
