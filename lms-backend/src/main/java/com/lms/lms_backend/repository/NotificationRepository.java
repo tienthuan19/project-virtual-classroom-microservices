4fab747b319ea60f890bf8ba2de44effdf4bf098
Le Bui Tien Thuan <lebuithuan@gmail.com>
2025-12-30 21:19:58 +0700

    refactor(lms/NotificationRepository): change to interface, add findByRecipientIdOrderByCreatedAtDesc list the notification

--- lms-backend/src/main/java/com/lms/lms_backend/repository/NotificationRepository.java
+++ lms-backend/src/main/java/com/lms/lms_backend/repository/NotificationRepository.java
@@ -1,7 +1,12 @@
 package com.lms.lms_backend.repository;
 
+import com.lms.lms_backend.models.Notification;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public class NotificationRepository {
+public interface NotificationRepository extends JpaRepository<Notification, String> {
+    List<Notification> findByRecipientIdOrderByCreatedAtDesc(String recipientId);
 }
