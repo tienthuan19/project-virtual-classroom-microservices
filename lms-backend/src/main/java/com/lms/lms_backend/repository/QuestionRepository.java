2f3fd55a9c8c886aa0eb258e75cfcb22314c8f7b
Le Bui Tien Thuan <lebuithuan@gmail.com>
2026-01-01 15:18:01 +0700

    feat(lms/QuestionRepository): Question repository

---
+++ lms-backend/src/main/java/com/lms/lms_backend/repository/QuestionRepository.java
@@ -0,0 +1,9 @@
+package com.lms.lms_backend.repository;
+
+import com.lms.lms_backend.models.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, String> {
+}
