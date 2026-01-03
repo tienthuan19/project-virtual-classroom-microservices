f4eaab707bb7dec387e4c737f8327101bb4619fe
Le Bui Tien Thuan <lebuithuan@gmail.com>
2026-01-04 01:24:18 +0700

    feat(lms/SubmissionDetailRepository)

---
+++ lms-backend/src/main/java/com/lms/lms_backend/repository/SubmissionDetailRepository.java
@@ -0,0 +1,7 @@
+package com.lms.lms_backend.repository;
+
+import com.lms.lms_backend.models.SubmissionDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubmissionDetailRepository extends JpaRepository<SubmissionDetail, Long> {
+}
