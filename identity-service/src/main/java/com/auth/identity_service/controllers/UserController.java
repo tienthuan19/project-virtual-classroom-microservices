cab21479acb3666b749ae2966231a9e202b47ef5
Le Bui Tien Thuan <lebuithuan@gmail.com>
2026-01-05 23:31:04 +0700

    feat(identity/UserController): /api/identity-service/v1/users/bulk-info Internal API

--- identity-service/src/main/java/com/auth/identity_service/controllers/UserController.java
+++ identity-service/src/main/java/com/auth/identity_service/controllers/UserController.java
@@ -1,5 +1,10 @@
 package com.auth.identity_service.controllers;
 
+import com.auth.identity_service.dto.responce.ApiResponse;
+import com.auth.identity_service.dto.responce.UserResponse;
+import com.auth.identity_service.services.UserService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,9 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.List;
+
 @RestController
 @RequestMapping(value = "/api/identity-service/v1/users")
 @RequiredArgsConstructor
 public class UserController {
-    //private final UserService userService; 
+    private final UserService userService;
+    // Internal API
+    @PostMapping("/bulk-info")
+    public ApiResponse<List<UserResponse>> getUsersByIds(@RequestBody List<String> userIds) {
+        return ApiResponse.<List<UserResponse>>builder()
+                .data(userService.getUsersByIds(userIds))
+                .build();
+    }
 }
