97088c26fa5d6568c8d78277a883dc8038c1c5ed
Le Bui Tien Thuan <lebuithuan@gmail.com>
2026-01-06 23:20:21 +0700

    feat(lms): EnableFeignClients

--- lms-backend/src/main/java/com/lms/lms_backend/LmsBackendApplication.java
+++ lms-backend/src/main/java/com/lms/lms_backend/LmsBackendApplication.java
@@ -2,8 +2,10 @@ package com.lms.lms_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class LmsBackendApplication {
 
 	public static void main(String[] args) {
